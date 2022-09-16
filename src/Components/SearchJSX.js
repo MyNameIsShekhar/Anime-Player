b1efed1f36971be6bf23ca21047d51ff09f1f890
Krishnendu <krishnendu.kalyani1@gmail.com>
2022-09-16 23:57:34 +0530

    Mobile View Fixed

--- src/Components/SearchJSX.js
+++ src/Components/SearchJSX.js
@@ -6,23 +6,52 @@ export default function SearchJSX(props) {
     props.handelClick();
   };
   return (
-    <div align="center">
-      <h4 className="green mt-3 fw-bold" style={{ fontFamily: "Poppins" }}>
-        SEARCH RESULT
-      </h4>
-      <span onClick={handelClick}>
-        {props.searchResult?.map((rec) => (
-          <Card rec={rec} key={rec.animeId} padding="0 9px" ep="false" />
-        ))}
-      </span>
-      <hr
-        style={{
-          color: "white",
-          height: "2px",
-          width: "80%",
-          margin: "20px auto",
-        }}
-      />
-    </div>
+    <>
+      {Object.keys(props.searchResult).length === 0 ? (
+        <div align="center">
+          <h4
+            className="mt-3 fw-bold"
+            style={{ fontFamily: "Poppins", color: "red" }}
+          >
+            NO SEARCH RESULT
+          </h4>
+          <hr
+            style={{
+              color: "white",
+              height: "2px",
+              width: "80%",
+              margin: "20px auto",
+            }}
+          />
+        </div>
+      ) : (
+        <div align="center">
+          <h4 className="green mt-3 fw-bold" style={{ fontFamily: "Poppins" }}>
+            SEARCH RESULT
+          </h4>
+          <hr
+            style={{
+              color: "white",
+              height: "2px",
+              width: "80%",
+              margin: "20px auto",
+            }}
+          />
+          <span onClick={handelClick}>
+            {props.searchResult?.map((rec) => (
+              <Card rec={rec} key={rec.animeId} padding="0 9px" ep="false" />
+            ))}
+          </span>
+          <hr
+            style={{
+              color: "white",
+              height: "2px",
+              width: "80%",
+              margin: "20px auto",
+            }}
+          />
+        </div>
+      )}
+    </>
   );
 }
